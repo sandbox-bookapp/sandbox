fef3946477a57669d4b290a906727fc3ecf12efd
TINA MYERS <myers.tina515@gmail.com>
2020-12-17 13:05:04 -0800

    one route working

--- src/server.js
+++ src/server.js
@@ -6,19 +6,33 @@ const notFoundHandler = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
 const logger = require('./middleware/logger.js');
 
-const v1Routes = require('./auth/routes/v1');
+// const v1Routes = require('./auth/routes/v1');
 const v2Routes = require('./auth/routes/v2');
 const authRoutes = require('./auth/routes.js');
 
+// new dependencies
+const methodOverride = require('method-override');
+// const superagent = require('superagent');
+const cors = require('cors');
+// const userLog = require('../auth/userLog');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
 
 app.use(logger);
 
 app.use(authRoutes);
-app.use('/api/v1', v1Routes);
+// app.use('/api/v1', v1Routes);
 app.use('/api/v2', v2Routes);
+
+// app.use('/user', userLog);
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
